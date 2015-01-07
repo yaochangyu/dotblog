b5987c2dce33d845eee8d6ecd4b11af87194c22b
余小章 <SND\yc421206_cp>
2015-01-07 06:53:01 +0000

    create file

---
+++ Simple.IdentityRole/Simple.IdentityRole/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Simple.IdentityRole.MvcApplication" Language="C#" %>
---
+++ Simple.IdentityRole/Simple.IdentityRole/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simple.IdentityRole.Startup))]
+namespace Simple.IdentityRole
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
